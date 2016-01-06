1112635ea9ce4a831558c6b788fa55c13a8db5ff
Dimitar Danailov <dimitar.danailov@mentormate.com>
2016-01-06 18:32:18 +0200

    Add project to source control system.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name microservicesExampleProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the microservicesExampleProjectApp
+ */
+angular.module('microservicesExampleProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name microservicesExampleProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the microservicesExampleProjectApp
+ */
+angular.module('microservicesExampleProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
